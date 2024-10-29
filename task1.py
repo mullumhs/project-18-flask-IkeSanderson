dc4df4b4c310fc09eac56c0fca609ddd9a55e14f
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-10-29 14:45:45 +1100

    Started the task

--- task1.py
+++ task1.py
@@ -2,8 +2,19 @@ from flask import Flask
 
 task1 = Flask(__name__)
 
+@task1.route('/')
 
+def index():
+
+    return 'Hello, World!'
+
+@task1.route('/hello/<Balls>')
+
+def hello(name):
+
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
 
     task1.run(debug=True)
+   
