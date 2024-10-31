da4036ad34de216467abe167e9dd0053c1693615
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-11-01 10:07:09 +1100

    figured out searching

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def greet(name):
     return render_template('greet.html', name=name)
 
 
-@app.route('/inventory')
-def inventory():
-    inventory_items = ['apple', 'banana', 'cherry']
-
-    return render_template('inventory.html', inventory = inventory_items)
+@app.route('/inventory/<thing>')
+def inventory(thing):
+    inventory_items = ['apple', 'banana', 'cherry','balls']
+    return render_template('inventory.html', inventory = inventory_items, thing = thing)
 
 @app.route('/test/<message>')
 def test(message):
